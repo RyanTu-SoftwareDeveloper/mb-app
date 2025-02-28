ac9b1fe3711d64acf58175f205c8cc7e871d9e55
Ryan Tu <ryantu@Mac.lan>
2025-02-27 17:00:03 -0800

    new change

--- posts/tests.py
+++ posts/tests.py
@@ -13,18 +13,18 @@ class PostModelTest(TestCase):
         self.assertEqual(expected_object_name, 'just a test')
 
 class HomePageViewTest(TestCase): # new
-    def setUp(self):
+    def setUp(self): #Each test starts with a fresh database (Django resets the database for each test)
         Post.objects.create(text='this is another test')
 
-    def test_view_url_exists_at_proper_location(self):
+    def test_view_url_exists_at_proper_location(self): #Test 1: URL Exists
         resp = self.client.get('/')
         self.assertEqual(resp.status_code, 200)
 
-    def test_view_url_by_name(self):
+    def test_view_url_by_name(self): #Test 2: URL Using Reverse Name
         resp = self.client.get(reverse('home'))
         self.assertEqual(resp.status_code, 200)
 
-    def test_view_uses_correct_template(self):
+    def test_view_uses_correct_template(self): #Test 3: Correct Template Used
         resp = self.client.get(reverse('home'))
         self.assertEqual(resp.status_code, 200)
         self.assertTemplateUsed(resp, 'home.html')
